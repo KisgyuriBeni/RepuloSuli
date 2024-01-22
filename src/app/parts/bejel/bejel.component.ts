c774c29974c5548081302ff4e335fe343ccf9822
KisgyorgyBendeguz <kisgyorgybeni@gmail.com>
2024-01-22 17:59:52 +0100

    teszt autentikáció működik!

--- src/app/parts/bejel/bejel.component.ts
+++ src/app/parts/bejel/bejel.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
@@ -7,11 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./bejel.component.css'],
 })
 export class BejelComponent {
-  
+  email: string = '';
+  password: string = '';
+  loggedInUserEmail: string = '';
+  isAuthenticated: boolean = false;
+  private authSubscription: Subscription;
   isSpinning: boolean = false;
 
-  startSpin() {
-    this.isSpinning = !this.isSpinning;
+  constructor(private authService: AuthService) {
+    this.authSubscription = this.authService.isAuthenticated().subscribe((value) => {
+      this.isAuthenticated = value;
+      if (this.isAuthenticated) {
+        // Ha be van jelentkezve, lekérjük a bejelentkezett felhasználó adatait
+        const loggedInUser = this.authService.getCurrentLoggedInUser().value;
+        if (loggedInUser) {
+          this.loggedInUserEmail = loggedInUser.email;
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+
+  login() {
+    this.isSpinning = true;
+
+    // Simuláció: bejelentkezés
+    setTimeout(() => {
+      if (this.authService.login(this.email, this.password)) {
+        // Bejelentkezett felhasználó nevének kiíratása a konzolba
+        console.log('Bejelentkezett felhasználó:', this.loggedInUserEmail);
+
+        // Mezők kiürítése
+        this.email = '';
+        this.password = '';
+        
+        // isSpinning leállítása 3 másodperc után
+        setTimeout(() => {
+          this.isSpinning = false;
+        }, 1000);
+      } else {
+        // Bejelentkezés sikertelen, isSpinning leállítása azonnal
+        this.isSpinning = false;
+      }
+    }, 1000);
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
 
--- src/app/parts/regist/regist.component.ts
+++ src/app/parts/regist/regist.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-regist',
@@ -7,9 +8,29 @@ import { Component} from '@angular/core';
 
 })
 export class RegistComponent {
+  email: string = '';
+  password: string = '';
+  username: string = ''
   isSpinning: boolean = false;
 
-  startSpin() {
-    this.isSpinning = !this.isSpinning;
+  constructor(private authService: AuthService) {}
+
+  register() {
+    this.isSpinning = true;
+
+    // Felhasználó hozzáadása
+    this.authService.addUser({ email: this.email, password: this.password, username:this.username });
+    
+    // Csak az input mezőket ürítjük ki
+    this.email = ''
+    this.password = ''
+    this.username = ''
+
+    console.log('Regisztráció sikeres:', this.username);
+
+    // Az isSpinning leállítása példa céljából 3 másodperc után
+    setTimeout(() => {
+      this.isSpinning = false;
+    }, 1000);
   }
 }
