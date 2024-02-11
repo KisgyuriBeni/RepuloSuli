502e9f3101ee718ce3a9cdee03844aee2d18d1da
MikesSzinyei <szinyeimikes@gmail.com>
2024-02-11 11:13:20 +0100

    testdata frissjtés

--- adatbázis scriptek és tervek/testdata.sql
+++ adatbázis scriptek és tervek/testdata.sql
@@ -1,17 +1,16 @@
-USE airplane;
+INSERT INTO `users` (`id`, `course_id`, `user_name`, `email`, `phone_number`, `password`, `first_name`, `last_name`, `mothers_name`, `address`, `birth_day`) VALUES
+(301, 201, 'gabi03', 'fazekas.gabi@gmail.com', '06304973654', 'Titok2024', 'Fazekas', 'Gábor', 'Szabó Melinda', '1055 Budapest, Kossuth Lajos tér 1-3', '2003-02-27'),
+(302, 202, 'endre1987', 'faragoendre@gmail.com', '06204987564', 'Titok2024', 'Farago', 'Endre', 'Fazekas Virág', '1077 Budapest, Hevesi Sándor tér 4', '1987-11-30');
 
-INSERT INTO airplanes
-VALUES (101, "Cessna 172","single-engine" ),
-       (102, "Cirrus SR20", "single-engine");
+INSERT INTO `courses` (`id`, `course_name`, `user_id`, `airplane_id`, `instructor`, `start_date`, `end_date`, `course_fee`) VALUES
+(201, 'Repülés kezdőknek', 301, 101, 'Kovács Balázs', '2023-12-15', '2024-02-15', 500000),
+(202, 'Repülés haladóknak', 302, 102, 'Kovács Balázs', '2024-01-20', '2024-03-20', 1200000);
 
-INSERT INTO courses
-VALUES (201, "Repülés kezdőknek", 301, 101,"Kovács Balázs", "2023/12/15", "2024/02/15", 500000 ),
-       (202, "Repülés haladóknak", 302, 102,"Kovács Balázs", "2024/01/20", "2024/03/20", 1200000 );
+INSERT INTO `airplanes` (`id`, `airplane_name`, `propulsion`) VALUES
+(101, 'Cessna 172', 'single-engine'),
+(102, 'Cirrus SR20', 'single-engine');
 
-INSERT INTO users
-VALUES(301, 201, "gabi03", "fazekas.gabi@gmail.com", "Titok2023", "Fazekas", "Gábor", "Szabó Melinda", "1055 Budapest, Kossuth Lajos tér 1-3", "2003/02/27" ),
-      (302, 202, "endre1987", "faragoendre@gmail.com", "Titok2023", "Farago", "Endre", "Fazekas Virág", "1077 Budapest, Hevesi Sándor tér 4", "1987/11/30");
-
-INSERT INTO users_courses
-VALUES (301, 201),
-       (302, 202);
+INSERT INTO `user_courses` (`user_id`, `course_id`) VALUES
+(301, 201),
+(302, 202);
+COMMIT;
