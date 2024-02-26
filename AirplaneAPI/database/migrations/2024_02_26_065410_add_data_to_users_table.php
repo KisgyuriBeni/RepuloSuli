dbd2c8fa4f1755fa23547f21b99510ffc5e13e53
MikesSzinyei <szinyeimikes@gmail.com>
2024-02-26 08:09:44 +0100

    migrations

---
+++ AirplaneAPI/database/migrations/2024_02_26_065410_add_data_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string("phone_number");
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->string("mothers_name");
+            $table->string("address");
+            $table->date("birth_day");
+            $table->integer("login_attempt")->default(0);
+            $table->timestamp("timeout")->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
