5493e3fbcacb86c17fc61547eb0edb425661c74c
KisgyorgyBendeguz <kisgyorgybeni@gmail.com>
2024-03-15 08:06:21 +0100

    felhasznalok adminon minden adat megjelenik a modalban

---
+++ AirplaneAPI/app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "user_name",
+        "email",
+        "phone_number",
+        "password",
+        "first_name",
+        "last_name",
+        "mothers_name",
+        "address",
+        "birth_day"
+    ];
+
+    
+    protected $hidden = [
+        'password',
+        'remember_token'
+        
+    ];
+
+    
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function courses(): BelongsToMany {
+
+        return $this->belongsToMany(Course::class, 'user_course', 'user_id', 'course_id');
+    }
+}
